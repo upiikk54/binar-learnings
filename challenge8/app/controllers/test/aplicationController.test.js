cee18dd6dff0db6e0dc7ef2b29a5299da69a8d0a
praditya luthfi <pradityaluthfi45@gmail.com>
2022-06-09 09:43:08 +0700

    testing with jest

---
+++ challenge8/app/controllers/test/aplicationController.test.js
@@ -0,0 +1,149 @@
+const {
+    NotFoundError
+} = require("../../errors");
+const ApplicationController = require("../ApplicationController");
+
+describe("handleGetRoot", () => {
+    it("should testing handleGetRoot", async () => {
+        const mockRequest = {};
+        const mockResponse = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        };
+
+        const payloadResponse = {
+            status: "OK",
+            message: "BCR API is up and running!"
+        };
+
+        const applicationController = new ApplicationController();
+
+        applicationController.handleGetRoot(mockRequest, mockResponse);
+
+        // Assertion
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json).toHaveBeenCalledWith(payloadResponse);
+    });
+});
+
+describe("handleNotFound", () => {
+    it("should testing handleNotFound", async () => {
+        const mockRequest = {};
+        const method = "GET";
+        const url = "http:localhost:8000/asd"
+
+        const mockResponse = {
+            status_code: 404,
+            status: () => {
+                return {
+                    json: () => {
+                        return {
+                            error: {
+                                name: "Error",
+                                message: "Not found!",
+                                details: {
+                                    method,
+                                    url,
+                                },
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        const err = new NotFoundError(method, url)
+
+        const applicationController = new ApplicationController();
+
+        applicationController.handleNotFound(mockRequest, mockResponse);
+
+        // Assertion
+        expect(mockResponse.status_code).toEqual(404);
+        expect(mockResponse.status().json()).toEqual({
+            error: {
+                name: err.name, 
+                message: err.message,
+                details: err.details,
+            }
+        });
+    });
+});
+
+describe("handleError", () => {
+    it("should testing handleError", async () => {
+        const mockRequest = {};
+        const mockNext = {};
+        const mockResponseHandleError = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        };
+
+        const err = new Error("something");
+
+        const applicationController = new ApplicationController();
+
+        applicationController.handleError(err, mockRequest, mockResponseHandleError, mockNext);
+
+        // Assertion
+        expect(mockResponseHandleError.status).toHaveBeenCalledWith(500);
+        expect(mockResponseHandleError.json).toHaveBeenCalledWith({
+            error: {
+                name: err.name,
+                message: err.message,
+                details: err.details || null,
+            }
+        });
+    });
+});
+
+describe("#getOffsetFromRequest", () => {
+    it("should call res.status(500) and res.json with task instance", () => {
+        const query = {
+            page: 1,
+            pageSize: 10
+        };
+
+        const mockRequest = {
+            query,
+        };
+
+        const offset = (query.page - 1) * query.pageSize;
+
+        const applicationController = new ApplicationController();
+        const result = applicationController.getOffsetFromRequest(mockRequest);
+
+        expect(result).toBe(offset);
+    });
+});
+
+describe("#buildPaginationObject", () => {
+    it("should call res.status(500) and res.json with task instance", () => {
+        const query = {
+            page: 1,
+            pageSize: 10
+        };
+        const count = 0;
+
+        const mockRequest = {
+            query,
+        };
+
+        const pageCount = Math.ceil(count / query.pageSize);
+
+        const kembali = [{
+            page: query.page,
+            pageCount,
+            pageSize: query.pageSize,
+            count,
+        }, ];
+
+        const applicationController = new ApplicationController();
+        const result = applicationController.buildPaginationObject(
+            mockRequest,
+            count
+        );
+
+        expect(result).toStrictEqual(kembali[0]);
+    });
+});
---
+++ challenge8/app/controllers/test/authenticationController.test.js
@@ -0,0 +1,49 @@
+const AuthenticationController = require("../AuthenticationController");
+
+describe('authorize', () => {
+    it('should testing authorize', () => {
+        
+    });
+});
+
+describe('handleLogin', () => {
+    it('should testing handleLogin', () => {
+        
+    });
+});
+
+describe('handleRegister', () => {
+    it('should testing handleRegister', () => {
+        
+    });
+});
+
+describe('handleGetUser', () => {
+    it('should testing handleGetUser', () => {
+        
+    });
+});
+
+describe('createTokenFromUser', () => {
+    it('should testing createTokenFromUser', () => {
+        
+    });
+});
+
+describe('decodeToken', () => {
+    it('should testing decodeToken', () => {
+        
+    });
+});
+
+describe('encryptPassword', () => {
+    it('should testing encryptPassword', () => {
+        
+    });
+});
+
+describe('verifyPassword', () => {
+    it('should testing verifyPassword', () => {
+        
+    });
+});
